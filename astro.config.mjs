5ad208bb9526cec1b2b4e4c35ac9b6f2582332cf
Harry Whorlow <harry.whorlow@technoly.com>
2024-01-02 12:48:01 +0100

    feat(tokens): update to variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://harry-whorlow.github.io',
   base: '/portfolio',
-  server: { port: 1234, host: true },
+  server: {
+    port: 1234,
+    host: true
+  },
+  integrations: [tailwind()]
 });
